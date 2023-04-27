37aea1786bd6886d6d8a2d3b2d9675c03108c621
Mehmet Uygun <mehmet.uygun@outlook.com>
2023-04-27 18:07:46 +0100

    Create library has not user rule

---
+++ app/Rules/LibraryHasNotUser.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Rules;
+
+use App\Models\Associate;
+use Closure;
+use Illuminate\Contracts\Validation\DataAwareRule;
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Translation\PotentiallyTranslatedString;
+
+class LibraryHasNotUser implements DataAwareRule, ValidationRule
+{
+    /**
+     * All the data under validation.
+     *
+     * @var array<string, mixed>
+     */
+    protected $data = [];
+
+    /**
+     * Set the data under validation.
+     *
+     * @param  array<string, mixed>  $data
+     */
+    public function setData(array $data): static
+    {
+        $this->data = $data;
+
+        return $this;
+    }
+
+    /**
+     * Run the validation rule.
+     *
+     * @param string $attribute
+     * @param mixed $value
+     * @param Closure(string): PotentiallyTranslatedString $fail
+     */
+    public function validate(
+        string $attribute,
+        mixed $value,
+        Closure $fail
+    ): void {
+        if (
+            Associate::whereLibraryId($this->data['library_id'])
+                ->whereUserId($this->data['user_id'])
+                ->exists()
+        ) {
+            $fail('This library has the user.');
+        }
+    }
+}
