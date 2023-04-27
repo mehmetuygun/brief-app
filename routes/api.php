bacc685245e55a6e879fe56cc6ad09769c017713
Mehmet Uygun <mehmet.uygun@outlook.com>
2023-04-27 18:09:33 +0100

    Create api routes

---
+++ routes/api.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Actions\Library\BorrowBook;
+use App\Actions\Library\RegisterAssociate;
+use App\Http\Controllers\BookController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('associates', RegisterAssociate::class)->name(
+        'associates.store'
+    );
+
+    Route::post('borrow/book', BorrowBook::class)->name('book.borrow');
+
+    Route::get('books', [BookController::class, 'index']);
+
+    Route::get('books/{book}', [BookController::class, 'show'])->name(
+        'books.show'
+    );
+});
